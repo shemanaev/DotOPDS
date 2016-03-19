5629182ffe94f2893393df572e67b20a3f3ebf51
Denis Shemanaev <denis.shemanaev@gmail.com>
2016-03-19 16:24:39 +0300

    Moved `PathUtil` to `Util` and added *nix platform detection.

---
+++ DotOPDS/Utils/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotOPDS.Utils
+{
+    class Util
+    {
+        public static string Normalize(string path)
+        {
+            var from = IsLinux ? '\\' : '/';
+            var to = IsLinux ? '/' : '\\';
+            return Environment.ExpandEnvironmentVariables(path).Replace(from, to);
+        }
+
+        public static bool IsLinux
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+    }
+}
