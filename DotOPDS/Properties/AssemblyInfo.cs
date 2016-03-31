87023eb3a4117638cc622de146cdcfbe6ccc1a55
Denis Shemanaev <denis.shemanaev@gmail.com>
2016-03-31 22:31:18 +0300

    Bring back AssemblyInfo.

---
+++ DotOPDS/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyTitle("Debug build")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyTitle("Lightweight OPDS server.")]
+#endif
+
+[assembly: AssemblyProduct("DotOPDS")]
+[assembly: AssemblyCopyright("(c) 2016 Denis Shemanaev")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
