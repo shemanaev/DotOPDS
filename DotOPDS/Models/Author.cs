15b4632543c0f74a692fa607059da57e8d7b3bfb
Denis Shemanaev <denis.shemanaev@gmail.com>
2017-02-06 18:26:56 +0300

    Take out methods from `Author` model.

--- DotOPDS/Models/Author.cs
+++ DotOPDS/Models/Author.cs
@@ -5,14 +5,5 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-
-        public string GetScreenName()
-        {
-            string format = "";
-            if (!string.IsNullOrEmpty(FirstName)) format += "{0}";
-            if (!string.IsNullOrEmpty(MiddleName)) format += " {1}";
-            if (!string.IsNullOrEmpty(LastName)) format += " {2}";
-            return string.Format(format, FirstName, MiddleName, LastName).Trim();
-        }
     }
 }
